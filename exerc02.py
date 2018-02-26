4efeaf50978cfab2afd432386030b4ad9c1a90b0
Thuany Mendonca <thuany1997@hotmail.com>
2018-02-26 10:00:35 -0300

    Aprendendo Python

---
+++ exerc02.py
@@ -0,0 +1,6 @@
+print('Desafio 2')
+dia = input('Dia = ')
+mes = input('Mês = ')
+ano = input('Ano = ')
+
+print('Data: ' + dia + '/' + mes + '/' + ano)
---
+++ teste01.py
@@ -0,0 +1,5 @@
+nome = input('Qual é o seu nome?')
+idade = input('Qual seu idade?')
+peso = input('Qual o seu peso?')
+
+print(nome, idade, peso)
